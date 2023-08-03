9f23e77013e10730914d6326d8ee03ddf9a12f65
sudhir <email sevenstar.onesignal@gmail.com>
2023-08-03 17:50:07 +0530

    123

--- app/src/main/java/com/my/note_app/multiplay.java
+++ app/src/main/java/com/my/note_app/multiplay.java
@@ -1,10 +1,17 @@
 package com.my.note_app;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 
 public class multiplay {
     @Override
     public boolean equals(@Nullable Object obj) {
         return super.equals(obj);
+    }
+
+    @NonNull
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
     }
 }
