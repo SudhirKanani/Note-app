50ca77a84beadb41a48a5484b6b379a8bbe2b028
sudhir <email sevenstar.onesignal@gmail.com>
2023-08-03 17:45:19 +0530

    nothing

--- app/src/main/java/com/my/note_app/multiplay.java
+++ app/src/main/java/com/my/note_app/multiplay.java
@@ -1,4 +1,10 @@
 package com.my.note_app;
 
+import androidx.annotation.Nullable;
+
 public class multiplay {
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        return super.equals(obj);
+    }
 }
