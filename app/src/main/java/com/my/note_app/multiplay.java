0410f6e1f51891b2adbe33d7e9b01f5acb33d616
sudhir <email sevenstar.onesignal@gmail.com>
2023-08-03 17:36:50 +0530

    some changes

--- app/src/main/java/com/my/note_app/multiplay.java
+++ app/src/main/java/com/my/note_app/multiplay.java
@@ -1,4 +1,11 @@
 package com.my.note_app;
 
+import androidx.annotation.Nullable;
+
 public class multiplay {
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        return super.equals(obj);
+    }
 }
